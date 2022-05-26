ab0f31389733b433cc9baaeef85d07118e1b6415
Eric Bannon <34694236+ericbannon@users.noreply.github.com>
2022-05-26 15:42:08 -0700

    Update imagecheck.sh

--- imagecheck.sh
+++ imagecheck.sh
@@ -2,5 +2,8 @@
 curl -s -k -L -H "Authorization: Bearer ${ROX_API_TOKEN}" https://${CENTRAL}/api/cli/download/roxctl-linux --output roxctl
 chmod +x roxctl
 
-# image check
+# image check 1
 ./roxctl image check -e ${CENTRAL}:443 --image "${IMAGE}" --insecure-skip-tls-verify
+
+# image check 2
+./roxctl image check -e ${CENTRAL}:443 --image "${IMAGE2}" --insecure-skip-tls-verify

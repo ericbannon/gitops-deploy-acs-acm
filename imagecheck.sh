d967afa854b0c0070fee04dce6e8f423df8b17e8
Eric Bannon <34694236+ericbannon@users.noreply.github.com>
2022-05-26 15:57:35 -0700

    Update imagecheck.sh

--- imagecheck.sh
+++ imagecheck.sh
@@ -4,6 +4,3 @@ chmod +x roxctl
 
 # image check 1
 ./roxctl image check -e ${CENTRAL}:443 --image "${IMAGE}" --insecure-skip-tls-verify
-
-# image check 2
-./roxctl image check -e ${CENTRAL}:443 --image "${IMAGE2}" --insecure-skip-tls-verify
